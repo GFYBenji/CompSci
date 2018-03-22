238b16fd6e072d11a23b3ef6b8cbc85ec46a6e3d
gfybenji@gmail.com <GFY280301b>
2018-03-22 22:56:30 +0000

    ouch

--- src/Screen.java
+++ src/Screen.java
@@ -1,52 +1,203 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
 
-class Screen extends JPanel implements MouseListener{
+class Screen extends JPanel implements MouseListener, ActionListener, KeyListener, WindowListener {
 
-    private MyImage I;
-    private JLabel main;
+    private MyImage mandelImage, mJImage, jImage;
+    private JLabel mandelLabel;
+    private JTextField txtRe;
+    private JTextField txtIm;
+    private JButton btnConfirm;
     private int xx, yy, width, height;
+    private JFrame juliaWindow, mJuliaWindow;
+    private Boolean julia, mJulia;
 
-    public Screen(MyImage image){
-        I = image;
+    public Screen(MyImage inImage) {
+        mandelImage = inImage;
         mandelFrame();
-        width = I.getWidth();
-        height = I.getHeight();
-        //juliaFrame(I);
+        width = mandelImage.getWidth();
+        height = mandelImage.getHeight();
+        julia = mJulia = false;
     }
 
     private void mandelFrame() {
-        JFrame window = new JFrame("MoodleBroot");
-        window.setBounds(0,0,I.getWidth(),I.getHeight());
-        window.setResizable(false);
-        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setLocationRelativeTo(null);
-        main = new JLabel();
-        main.setBounds(0,0,I.getWidth(),I.getHeight());
-        main.setIcon(new ImageIcon(I));
-        main.addMouseListener(this);
-        main.setFocusable(true);
-        window.add(main);
-        window.setVisible(true);
+        JFrame mandelWindow = new JFrame("MoodleBroot");
+        mandelWindow.setBounds(0, 0, mandelImage.getWidth(), mandelImage.getHeight());
+        mandelWindow.setResizable(false);
+        mandelWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mandelWindow.setLocationRelativeTo(null);
+        mandelLabel = new JLabel();
+        mandelLabel.setBounds(0, 0, mandelImage.getWidth(), mandelImage.getHeight());
+        mandelLabel.setIcon(new ImageIcon(mandelImage));
+        mandelLabel.addMouseListener(this);
+        mandelLabel.addKeyListener(this);
+        mandelLabel.setFocusable(true);
+        mandelWindow.add(mandelLabel);
+        mandelWindow.setVisible(true);
     }
 
     private void juliaFrame(MyImage image) {
-        JFrame window2 = new JFrame("Jellybread");
-        window2.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        window2.setBounds(0, 0, image.getWidth(), image.getHeight());
-        window2.setResizable(false);
-        window2.setLocationRelativeTo(null);
-        JLabel main2 = new JLabel();
-        main2.setBounds(0, 0, window2.getWidth(), window2.getHeight());
-        main2.setIcon(new ImageIcon(image));
-        window2.add(main2);
-        window2.setVisible(true);
+        juliaWindow = new JFrame("Jellybread");
+        juliaWindow.addWindowListener(this);
+        juliaWindow.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        juliaWindow.setBounds(0, 0, image.getWidth(), image.getHeight());
+        juliaWindow.setResizable(false);
+        juliaWindow.setLocationRelativeTo(null);
+        JLabel juliaLabel = new JLabel();
+        juliaLabel.setBounds(0, 0, juliaWindow.getWidth(), juliaWindow.getHeight());
+        juliaLabel.setIcon(new ImageIcon(image));
+        juliaWindow.add(juliaLabel);
+        juliaWindow.setVisible(true);
+        julia = true;
+    }
+
+    private void manualJulia() {
+        JLabel lblRe;
+        JLabel lblIm;
+        JLabel lblTitle;
+        JLabel lblSpacer;
+
+        mJuliaWindow = new JFrame("Juliaset Creator");
+        JPanel jpanel1 = new JPanel();
+
+        jpanel1.setBorder(BorderFactory.createTitledBorder("Choose Your Values"));
+
+        GridBagLayout gbManualJulia = new GridBagLayout();
+        GridBagConstraints gbcManualJulia = new GridBagConstraints();
+        jpanel1.setLayout(gbManualJulia);
+
+        btnConfirm = new JButton("Go!");
+        btnConfirm.addActionListener(this);
+        gbcManualJulia.gridx = 6;
+        gbcManualJulia.gridy = 14;
+        gbcManualJulia.gridwidth = 9;
+        gbcManualJulia.gridheight = 2;
+        gbcManualJulia.fill = GridBagConstraints.BOTH;
+        gbcManualJulia.weightx = 1;
+        gbcManualJulia.weighty = 0;
+        gbcManualJulia.anchor = GridBagConstraints.NORTH;
+        gbManualJulia.setConstraints(btnConfirm, gbcManualJulia);
+        jpanel1.add(btnConfirm);
+
+        txtRe = new JTextField();
+        txtRe.setToolTipText("Real Value for Juliaset");
+        gbcManualJulia.gridx = 6;
+        gbcManualJulia.gridy = 12;
+        gbcManualJulia.gridwidth = 1;
+        gbcManualJulia.gridheight = 1;
+        gbcManualJulia.fill = GridBagConstraints.BOTH;
+        gbcManualJulia.weightx = 1;
+        gbcManualJulia.weighty = 0;
+        gbcManualJulia.anchor = GridBagConstraints.NORTHWEST;
+        gbManualJulia.setConstraints(txtRe, gbcManualJulia);
+        jpanel1.add(txtRe);
+
+        txtIm = new JTextField();
+        txtIm.setToolTipText("Imaginary Value for Juliaset");
+        gbcManualJulia.gridx = 14;
+        gbcManualJulia.gridy = 12;
+        gbcManualJulia.gridwidth = 1;
+        gbcManualJulia.gridheight = 1;
+        gbcManualJulia.fill = GridBagConstraints.BOTH;
+        gbcManualJulia.weightx = 1;
+        gbcManualJulia.weighty = 0;
+        gbcManualJulia.anchor = GridBagConstraints.NORTH;
+        gbManualJulia.setConstraints(txtIm, gbcManualJulia);
+        jpanel1.add(txtIm);
+
+        lblRe = new JLabel("Real Value:");
+        gbcManualJulia.gridx = 5;
+        gbcManualJulia.gridy = 12;
+        gbcManualJulia.gridwidth = 1;
+        gbcManualJulia.gridheight = 1;
+        gbcManualJulia.fill = GridBagConstraints.BOTH;
+        gbcManualJulia.weightx = 1;
+        gbcManualJulia.weighty = 1;
+        gbcManualJulia.anchor = GridBagConstraints.NORTH;
+        gbManualJulia.setConstraints(lblRe, gbcManualJulia);
+        jpanel1.add(lblRe);
+
+        lblIm = new JLabel("Imaginary Value:");
+        gbcManualJulia.gridx = 13;
+        gbcManualJulia.gridy = 12;
+        gbcManualJulia.gridwidth = 1;
+        gbcManualJulia.gridheight = 1;
+        gbcManualJulia.fill = GridBagConstraints.BOTH;
+        gbcManualJulia.weightx = 1;
+        gbcManualJulia.weighty = 1;
+        gbcManualJulia.anchor = GridBagConstraints.NORTHEAST;
+        gbManualJulia.setConstraints(lblIm, gbcManualJulia);
+        jpanel1.add(lblIm);
+
+        lblTitle = new JLabel("Manual Juliaset Calculator");
+        gbcManualJulia.gridx = 5;
+        gbcManualJulia.gridy = 3;
+        gbcManualJulia.gridwidth = 10;
+        gbcManualJulia.gridheight = 1;
+        gbcManualJulia.fill = GridBagConstraints.VERTICAL;
+        gbcManualJulia.weightx = 1;
+        gbcManualJulia.weighty = 1;
+        gbcManualJulia.anchor = GridBagConstraints.NORTH;
+        gbManualJulia.setConstraints(lblTitle, gbcManualJulia);
+        jpanel1.add(lblTitle);
+
+        lblSpacer = new JLabel("Spacer Is here");
+        lblSpacer.setForeground(new Color(240, 240, 240));
+        gbcManualJulia.gridx = 7;
+        gbcManualJulia.gridy = 12;
+        gbcManualJulia.gridwidth = 6;
+        gbcManualJulia.gridheight = 1;
+        gbcManualJulia.fill = GridBagConstraints.BOTH;
+        gbcManualJulia.weightx = 1;
+        gbcManualJulia.weighty = 1;
+        gbcManualJulia.anchor = GridBagConstraints.NORTH;
+        gbManualJulia.setConstraints(lblSpacer, gbcManualJulia);
+        jpanel1.add(lblSpacer);
+
+        mJuliaWindow.add(jpanel1);
+        mJuliaWindow.pack();
+        mJuliaWindow.setBounds(0, 0, 800, mJuliaWindow.getHeight());
+        mJuliaWindow.setLocationRelativeTo(null);
+        mJuliaWindow.setVisible(true);
+        mJulia = true;
+    }
+
+    private void saveItems() {
+
     }
 
     @Override
-    public void mouseClicked(MouseEvent e) {
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnConfirm) {
+            double re, im;
+            re = Double.valueOf(txtRe.getText());
+            im = Double.valueOf(txtIm.getText());
+            mJImage = new MyImage(width, height, MyImage.TYPE_INT_RGB);
+            mJImage.Plot(-2.0, 2.0, 2.0, -2.0);
+            Calculator calc = new Calculator(mJImage, 200);
+            juliaFrame(calc.juliaSet(re, im));
+        }
+    }
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyChar() == 'j') {
+            manualJulia();
+        }
+        if (e.getKeyChar() == 's') {
+            //Save Function
+            try {
+                File output = new File("saved.png");
+                ImageIO.write(jImage, "png", output);
+                System.out.println("Saved!");
+            } catch (IOException j) {
+                System.out.println("Oof");
+            }
+        }
     }
 
     @Override
@@ -59,24 +210,32 @@ class Screen extends JPanel implements MouseListener{
     public void mouseReleased(MouseEvent e) {
         double StartX, StartY, EndX, EndY, re, im;
         if (xx == e.getX() || yy == e.getY()) {
-            MyImage image = new MyImage(width, height, MyImage.TYPE_INT_RGB);
-            image.Plot(-2.0, 2.0, 2.0, -2.0);
-            re = image.convertX(xx);
-            im = image.convertY(yy);
-            Calculator calc = new Calculator(image, 200);
-            juliaFrame(calc.juliaSet(re, im));
+            jImage = new MyImage(width, height, MyImage.TYPE_INT_RGB);
+            jImage.Plot(-2.0, 2.0, 2.0, -2.0);
+            re = jImage.convertX(xx);
+            im = jImage.convertY(yy);
+            Calculator calc = new Calculator(jImage, 200);
+            //juliaFrame(calc.juliaSet(re, im));
         } else {
-            StartX = I.convertX(xx);
-            StartY = I.convertY(yy);
-            EndX = I.convertX(e.getX());
-            EndY = I.convertY(e.getY());
-            I.Plot(StartX, StartY, EndX, EndY);
-            Calculator calc = new Calculator(I, 200);
-            main.setIcon(new ImageIcon(calc.mandelBrot()));
+            StartX = mandelImage.convertX(xx);
+            StartY = mandelImage.convertY(yy);
+            EndX = mandelImage.convertX(e.getX());
+            EndY = mandelImage.convertY(e.getY());
+            mandelImage.Plot(StartX, StartY, EndX, EndY);
+            Calculator calc = new Calculator(mandelImage, 200);
+            mandelLabel.setIcon(new ImageIcon(calc.mandelBrot()));
             repaint();
         }
 
     }
+
+    //region NotAbstractClass
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+
     @Override
     public void mouseEntered(MouseEvent e) {
         //When the mouse enters the JFrame
@@ -87,4 +246,55 @@ class Screen extends JPanel implements MouseListener{
         //When the mouse leaves the JFrame
         //System.out.println("Help");
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        if (e.getSource() == juliaWindow) {
+            julia = false;
+        }
+        if (e.getSource() == mJuliaWindow) {
+            mJulia = false;
+        }
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+    //endregion
 }
